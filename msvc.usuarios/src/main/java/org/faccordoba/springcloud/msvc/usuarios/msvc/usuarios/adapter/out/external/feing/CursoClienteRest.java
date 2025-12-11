3ea239c28e18c40f5c83298b18be92312202bab9
Facundo cordoba <cordobafs@gmail.com>
2025-12-11 12:18:55 -0300

    update springboot

--- msvc.usuarios/src/main/java/org/faccordoba/springcloud/msvc/usuarios/msvc/usuarios/adapter/out/external/feing/CursoClienteRest.java
+++ msvc.usuarios/src/main/java/org/faccordoba/springcloud/msvc/usuarios/msvc/usuarios/adapter/out/external/feing/CursoClienteRest.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "msvc-cursos", url= "${msvc.cursos.url}")
+@FeignClient(name = "msvc-cursos")
 public interface CursoClienteRest {
     @DeleteMapping("/api/v1/cursos/usuarios/{id}")
     void eliminarCursoUsuario(@PathVariable("id") Long id);
